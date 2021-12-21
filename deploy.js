9447c47cdf13facdcd0cc7d875a8ac11f54482f4
ashrafstakala <ashrafstakala@gmail.com>
2021-12-21 17:22:02 +0200

    inbox contract upgrade & refactor

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+
+const { abi, evm } = require('./compile');
+
+provider = new HDWalletProvider(
+  'crack afford tattoo silver gadget unfair problem broccoli execute grit rich merge', // test Metamask wallet with test ETH
+  'https://rinkeby.infura.io/v3/b63698a4df7c4a2497d508687d5bd4dd' // Infura API endpoint
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
